7a3d323f4778340cbd9737adb5aba960d4a5ee7b
aris <arisvomv@gmail.com>
2024-10-14 00:36:01 +0300

    dsf

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,48 @@
 <template>
+  <div>
+    Hellos: {{ text }}
 
-Hello:
-{{ text }}
-
-<gng-form-control v-model="text" />
-  <!-- :type="field.type" 
-  state="warning" 
-  :label="field.name" 
-  
-  placeholder="text placeholder" 
-  :message="field.value" -->
-
-<!-- 
-{{ code }}
-
-{{ text }}
-    <gng-form-element class="koko" type="password" label="text" v-model="text" placeholder="text placeholder" state="info" />
-
-    {{ checkbox }}
-    <gng-form-element type="checkbox" state="info" label="checkbox" v-model="checkbox" value="koko" />
-
-    <gng-form-control class="koko" type="text" state="warning" label="text" v-model="text" placeholder="text placeholder" message="text message" />
-    <gng-form-control type="password" state="error" label="text" v-model="text" placeholder="text placeholder" message="text message" disabled /> -->
-
+    <!-- Example usage of gng-form-control component -->
+    <gng-form-control state="error" message="pipi" label="mylae" v-model="text" />
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { inject } from 'vue';
-// import Navigation from './components/Navigation.vue';
-// import { fetchPage, updatePage, fetchPageProperty, createPage } from '@services/page.js'
-// import {fetchBlock, fetchBlockChildren, appendBlock, updateBlock, deleteBlock } from '@services/block.js'
-// import {fetchDatabase, queryDatabase } from '@services/databases.js'
+import { ref, inject } from 'vue';
+
 export default {
-  components: {
-    // Navigation
-  },
   setup() {
-    const pages = inject('pages', []);
+    // Injected dependencies or props
+    const pages = inject('pages', []); // Injected from parent context
+
     return {
-      pages,
-      formfields: [],
+      pages,  // Injected data
+      formfields: [], // Empty array for form fields
     };
   },
+
   mounted() {
-    console.log('Yo mounted');
-    // fetchBlockChildren(this.adminId).then(response => {
-    //   console.log('fetchBlockChildren',response);
-    //   this.adminBlockChildren = response.results;
+    console.log('Component mounted');
+
+    // Example of fetching data or making API calls can go here
+    // Uncomment and adjust the following as needed
+    // fetchDatabase('DATABASE_ID').then(response => {
+    //   console.log('fetchDatabase', response);
     // });
-
-    // fetchDatabase('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
-    //     console.log('fetchDatabase',response);
-    //   });
-
-    // queryDatabase('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
-    //   console.log('queryDatabase');
-      
-    //   console.log(response);
-    // });
-
-    // fetchBlockChildren('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
-    //   console.log(response);
-    //   this.formfields = response.results;
-    // });
-
   },
+
   data() {
     return {
-      adminId: '8a4268397a4545dca8910d682c84715f',
-      adminBlockChildren: [],
-
-      text: null,
-      textarea: null,
-      checkbox: [],
-      radio: null,
-      color: null,
-      date: null,
-      datetime: null,
-      email: null,
-      month: null,
-      number: null,
-      password: null,
-      range: null,
-      tel: null,
-      time: null,
-      week: null,
-      url: null,
-      button: null,
-      file: null,
-      hidden: null,
-      image: null,
-      reset: null,
-      search: null,
-      submit: null
+      text: null, // Example text data for form input
+      adminId: '8a4268397a4545dca8910d682c84715f', // Your adminId (if needed)
+      adminBlockChildren: [], // Placeholder for block children (if needed)
     };
   },
-  computed: {
-    // fields() {
-    //   let x = this.adminBlockChildren.find(el => el.type == 'code');
-    //   if (x) {
-    //     let str = x.code.text[0].text.content
-    //     console.log(str);
-        
-    //     let fields = JSON.parse(str)
-    //     console.log(fields);
-    //     return fields
-    //   }
-    //   return []
-    // }
-  },
-
 };
 </script>
 
 <style lang="scss">
-
- .yp > * {
+.yp > * {
   margin-bottom: 20px;
- }
+}
 </style>
---
+++ src/App_old.vue
@@ -0,0 +1,122 @@
+<template>
+
+Hello:
+{{ text }}
+
+<gng-form-control state="error" message="pipi" label="mylae" v-model="text" />
+  <!-- :type="field.type" 
+  state="warning" 
+  :label="field.name" 
+  
+  placeholder="text placeholder" 
+  :message="field.value" -->
+
+<!-- 
+{{ code }}
+
+{{ text }}
+    <gng-form-element class="koko" type="password" label="text" v-model="text" placeholder="text placeholder" state="info" />
+
+    {{ checkbox }}
+    <gng-form-element type="checkbox" state="info" label="checkbox" v-model="checkbox" value="koko" />
+
+    <gng-form-control class="koko" type="text" state="warning" label="text" v-model="text" placeholder="text placeholder" message="text message" />
+    <gng-form-control type="password" state="error" label="text" v-model="text" placeholder="text placeholder" message="text message" disabled /> -->
+
+</template>
+
+<script>
+import { ref } from 'vue';
+import { inject } from 'vue';
+// import Navigation from './components/Navigation.vue';
+// import { fetchPage, updatePage, fetchPageProperty, createPage } from '@services/page.js'
+// import {fetchBlock, fetchBlockChildren, appendBlock, updateBlock, deleteBlock } from '@services/block.js'
+// import {fetchDatabase, queryDatabase } from '@services/databases.js'
+export default {
+  components: {
+    // Navigation
+  },
+  setup() {
+    const pages = inject('pages', []);
+    return {
+      pages,
+      formfields: [],
+    };
+  },
+  mounted() {
+    console.log('Yo mounted');
+    // fetchBlockChildren(this.adminId).then(response => {
+    //   console.log('fetchBlockChildren',response);
+    //   this.adminBlockChildren = response.results;
+    // });
+
+    // fetchDatabase('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
+    //     console.log('fetchDatabase',response);
+    //   });
+
+    // queryDatabase('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
+    //   console.log('queryDatabase');
+      
+    //   console.log(response);
+    // });
+
+    // fetchBlockChildren('11db4ea9-8b62-8017-9992-e56740d566d0').then(response => {
+    //   console.log(response);
+    //   this.formfields = response.results;
+    // });
+
+  },
+  data() {
+    return {
+      adminId: '8a4268397a4545dca8910d682c84715f',
+      adminBlockChildren: [],
+
+      text: null,
+      textarea: null,
+      checkbox: [],
+      radio: null,
+      color: null,
+      date: null,
+      datetime: null,
+      email: null,
+      month: null,
+      number: null,
+      password: null,
+      range: null,
+      tel: null,
+      time: null,
+      week: null,
+      url: null,
+      button: null,
+      file: null,
+      hidden: null,
+      image: null,
+      reset: null,
+      search: null,
+      submit: null
+    };
+  },
+  computed: {
+    // fields() {
+    //   let x = this.adminBlockChildren.find(el => el.type == 'code');
+    //   if (x) {
+    //     let str = x.code.text[0].text.content
+    //     console.log(str);
+        
+    //     let fields = JSON.parse(str)
+    //     console.log(fields);
+    //     return fields
+    //   }
+    //   return []
+    // }
+  },
+
+};
+</script>
+
+<style lang="scss">
+
+ .yp > * {
+  margin-bottom: 20px;
+ }
+</style>
